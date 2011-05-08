5cf77c917d1c0cb2f8ca767efe8ba2b4c33611df
Vijfhoek <vijfhoek35@gmail.com>
2011-05-08 22:05:44 +0200

    added some empty methods

--- src/com/bot42/Bot42.java
+++ src/com/bot42/Bot42.java
@@ -1,7 +1,17 @@
 package com.bot42;
 
 public class Bot42 {
+	public String nick = "Bot42";
+	
 	public static void main(String[] args) {
 		
+	}
+	
+	public static void send(String line) {
+		
+	}
+	
+	public static String read() {
+		return "";
 	}
 }

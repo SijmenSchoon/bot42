31ce0ca361b9a08184f10f164bcd1f72ad3ae628
Vijfhoek <vijfhoek35@gmail.com>
2011-05-08 23:44:12 +0200

    added .kick command, added isChannel()

--- src/com/bot42/Bot42.java
+++ src/com/bot42/Bot42.java
@@ -11,17 +11,19 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Bot42 {
-	private static String nick = "Bot42";
-	private static String host = "irc.kottnet.net";
-	private static int port = 6667;
-	private static Socket ircSocket;
+	private String nick = "Bot42";
+	private String host = "irc.kottnet.net";
+	private int port = 6667;
+	private Socket ircSocket;
 	
-	private static PrintWriter ircWriter = null;
-	private static BufferedReader ircReader = null;
+	private PrintWriter ircWriter = null;
+	private BufferedReader ircReader = null;
 	
-	private static List<String> joinedChannels = new LinkedList<String>();
-	private static List<String> globalOps = new LinkedList<String>();
-	private static HashMap<String, List<String>> channelOps = new HashMap<String, List<String>>();
+	private List<String> joinedChannels = new LinkedList<String>();
+	private List<String> globalOps = new LinkedList<String>();
+	private HashMap<String, List<String>> channelOps = new HashMap<String, List<String>>();
+	
+	private static Bot42 bot42 = new Bot42();
 	
 	public static void main(String[] args) {
 		System.out.println("Bot42 IRC Bot by Vijfhoek and F16Gaming.");
@@ -31,37 +33,38 @@ public class Bot42 {
 		
 		boolean connected = false;
 		
+		
 		// TODO Add a config file for this
-		globalOps.add("Vijfhoek");
-		globalOps.add("F16Gaming");
+		bot42.globalOps.add("Vijfhoek");
+		bot42.globalOps.add("F16Gaming");
 		
 		try {
-			ircSocket = new Socket(host, port);
-			ircWriter = new PrintWriter(ircSocket.getOutputStream());
-			ircReader = new BufferedReader(new InputStreamReader(ircSocket.getInputStream()));
+			bot42.ircSocket = new Socket(bot42.host, bot42.port);
+			bot42.ircWriter = new PrintWriter(bot42.ircSocket.getOutputStream());
+			bot42.ircReader = new BufferedReader(new InputStreamReader(bot42.ircSocket.getInputStream()));
 			
-			write("NICK " + nick);
-			write("USER " + nick.toLowerCase() + " 0 * :" + nick);
+			bot42.write("NICK " + bot42.nick);
+			bot42.write("USER " + bot42.nick.toLowerCase() + " 0 * :" + bot42.nick);
 			
 			while (true) {
-				String message = read();
+				String message = bot42.read();
 				String[] splitMessage = message.split(" ");
 				
 				if (splitMessage[0].equals("PING")) {
-					write("PONG " + splitMessage[1]);
+					bot42.write("PONG " + splitMessage[1]);
 				}
 				if (!connected) {
 					if (splitMessage[1].equals("376")) {
 						connected = true;
-						write("JOIN #Bot42");
+						bot42.write("JOIN #Bot42");
 					} else if (splitMessage[1].equals("433")) {
-						nick = nick + "|2";
-						write("NICK " + nick);
+						bot42.nick += "|2";
+						bot42.write("NICK " + bot42.nick);
 					}
 				}
 				
 				if (splitMessage[1].equals("366")) {
-					joinedChannels.add(splitMessage[3]);
+					bot42.joinedChannels.add(splitMessage[3]);
 				} else if (splitMessage[1].equals("353")) {
 					List<String> ops = new LinkedList<String>(); 
 					for (int i = 5; i < splitMessage.length; i++) {
@@ -70,33 +73,46 @@ public class Bot42 {
 							ops.add(targetNick.substring(1));
 						}
 					}
-					channelOps.put(splitMessage[4], ops);
+					bot42.channelOps.put(splitMessage[4], ops);
 				}
 				
-				if (splitMessage[1].equals("KICK") && splitMessage[3].equals(nick)) {
-					joinedChannels.remove(splitMessage[2]);
+				if (splitMessage[1].equals("KICK") && splitMessage[3].equals(bot42.nick)) {
+					bot42.joinedChannels.remove(splitMessage[2]);
 				} else if (splitMessage[1].equals("PRIVMSG")) {
-					if (isOp(hostToNick(splitMessage[0]), splitMessage[2])) {
+					if (bot42.isOp(bot42.hostToNick(splitMessage[0]), splitMessage[2])) {
 						if (splitMessage[3].equals(":.print")) {
 							String buffer = "";
 							for (int i = 4; i < splitMessage.length; i++) {
 								buffer += splitMessage[i] + " ";
 							}
 							buffer = buffer.trim();
-							write("PRIVMSG " + splitMessage[2] + " :" + buffer);
+							bot42.write("PRIVMSG " + splitMessage[2] + " :" + buffer);
 						} else if (splitMessage[3].equals(":.raw")) {
 							String buffer = "";
 							for (int i = 4; i < splitMessage.length; i++) {
 								buffer += splitMessage[i] + " ";
 							}
 							buffer = buffer.trim();
-							write(buffer);
+							bot42.write(buffer);
+						} else if (splitMessage[3].equals(":.kick")) {
+							String channel;
+							String target;
+							
+							if (bot42.isChannel(splitMessage[2])) {
+								channel = splitMessage[2];
+								target = splitMessage[4];
+							} else {
+								channel = splitMessage[4];
+								target = splitMessage[5];
+							}
+							
+							bot42.write("KICK " + channel + " " + target + " :(" + bot42.hostToNick(splitMessage[0]) + ")");
 						}
 					}
 				}
 			}
 		} catch (UnknownHostException e) {
-			System.out.println("[ERR] Can't connect to host " + host + ":" + port);
+			System.out.println("[ERR] Can't connect to host " + bot42.host + ":" + bot42.port);
 			return;
 		} catch (IOException e) {
 			System.out.println("[ERR] IO Exception " + e + " occured");
@@ -104,20 +120,20 @@ public class Bot42 {
 		}
 	}
 	
-	public static void write(String line) {
+	public void write(String line) {
 		ircWriter.print(line + "\r\n");
 		ircWriter.flush();
 		System.out.println("[OUT] " + line);
 	}
 	
-	public static String read() throws IOException {
+	public String read() throws IOException {
 		String line = "";
 		line = ircReader.readLine();
 		System.out.println(" [IN] " + line);
 		return line;
 	}
 	
-	public static boolean isOp(String nick, String channel) {
+	public boolean isOp(String nick, String channel) {
 		if (channel.startsWith("#"))
 		{
 			if (!channelOps.containsKey(channel))
@@ -127,7 +143,7 @@ public class Bot42 {
 				return false;
 			else
 				return true;
-		} else if (channel.equals(Bot42.nick)) {
+		} else if (channel.equals(bot42.nick)) {
 			if (!globalOps.contains(nick))
 				return false;
 			return true;
@@ -135,7 +151,13 @@ public class Bot42 {
 		return false;
 	}
 	
-	public static String hostToNick(String host) {
+	public boolean isChannel(String target) {
+		if (target.startsWith("#"))
+			return true;
+		return false;
+	}
+	
+	public String hostToNick(String host) {
 		host = host.replaceFirst(":", "");
 		if (!host.contains("!"))
 			return null;

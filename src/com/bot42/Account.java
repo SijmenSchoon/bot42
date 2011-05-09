62482c52225924fbdff9c4514d0cf30c90beb779
Vijfhoek <vijfhoek35@gmail.com>
2011-05-09 08:11:43 +0200

    added checkPassword() method

--- src/com/bot42/Account.java
+++ src/com/bot42/Account.java
@@ -1,7 +1,9 @@
 package com.bot42;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -31,5 +33,24 @@ public class Account {
 		writer.write("password=\"" + passhash + "\"\n");
 		writer.flush(); writer.close();
 		return true;
+	}
+	
+	public boolean checkPassword (String pass) throws Exception {
+		if (!location.exists()) return false;
+		String passhash = String.valueOf(pass.hashCode());
+		BufferedReader reader = new BufferedReader(new FileReader(location));
+		String configPasshash = "";
+		while (reader.ready()) {
+			String line = reader.readLine();
+			if (line.contains("password=")) {
+				configPasshash = line.split("=")[1].substring(1, line.split("=").length-2);
+				break;
+			}
+		}
+		
+		if (passhash.equals(String.valueOf(configPasshash))) {
+			return true;
+		}
+		return false;
 	}
 }

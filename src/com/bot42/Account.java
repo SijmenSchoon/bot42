8496bda89e957d51a25cb943e8e8f18726d9790f
Vijfhoek <vijfhoek35@gmail.com>
2011-05-09 02:21:27 +0200

    fixed a little bug

--- src/com/bot42/Account.java
+++ src/com/bot42/Account.java
@@ -6,13 +6,15 @@ import java.io.FileWriter;
 
 public class Account {
 	private File location;
+	private String username;
 	
 	public Account (String username) {
 		(new File("accounts")).mkdirs();
 		location = new File("accounts" + File.separator + username + "cfg");
+		this.username = username;
 	}
 	
-	public boolean addUser (String username) throws Exception {
+	public boolean addUser () throws Exception {
 		if (location.exists()) return false;
 		location.createNewFile();
 		BufferedWriter writer = new BufferedWriter(new FileWriter(location));
